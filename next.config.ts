b2bbcf895e5352d7a185bfd3a3c69979ea02eeda
jeongbaebang <jeongbaebang_dev@naver.com>
2025-03-25 18:35:11 +0900

    fix 8

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'www.chokoppang.com',
+        'chokoppang.com',
+        'localhost:3000',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
