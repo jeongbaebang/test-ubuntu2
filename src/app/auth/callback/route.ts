8098cac12e0e88b1b300160b5ad5f0c996bc7a19
jeongbaebang <jeongbaebang_dev@naver.com>
2025-03-25 16:39:18 +0900

    fix 4

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,31 +4,26 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const headersList = await headers();
-  const origin = headersList.get('origin') || process.env.NEXT_PUBLIC_SITE_URL;
+  const requestUrl = new URL(request.url);
+  const origin =
+    headersList.get('origin') || (process.env.NEXT_PUBLIC_SITE_URL as string);
+  const code = requestUrl.searchParams.get('code');
+  const next = requestUrl.searchParams.get('next') || '/';
 
-  try {
-    const { searchParams } = new URL(request.url);
-    const code = searchParams.get('code');
+  console.log('export async function GET(request: NextRequest) {');
+  console.log('request', request.url);
+  console.log('request', origin);
+  console.log('end--');
 
-    console.log('export async function GET(request: NextRequest) {');
-    console.log('request', request.url);
-    console.log('request', origin);
-    console.log('end--');
-
-    if (!code) throw new Error('Authorization code missing');
-
+  if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
-    console.log(error);
-
-    return error
-      ? NextResponse.redirect(`${origin}/error?code=auth_fail`)
-      : NextResponse.redirect(new URL(searchParams.get('next') || '/', origin));
-  } catch (error) {
-    console.error('Auth callback error:', error);
-    return NextResponse.redirect(
-      new URL(`/error?code=internal_error`, process.env.NEXT_PUBLIC_SITE_URL)
-    );
+    if (!error) {
+      return NextResponse.redirect(new URL(next, origin));
+    }
   }
+
+  // 오류 페이지로 리디렉션
+  return NextResponse.redirect(new URL('/error', origin));
 }
