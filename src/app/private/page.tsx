47d09ec60ef681a3e212b87c7c6836e2c13e8ed8
jeongbaebang <jeongbaebang_dev@naver.com>
2025-03-29 01:41:39 +0900

    fix 25

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -6,17 +6,17 @@ export default async function PrivatePage() {
   const supabase = await createClient();
   const {
     data: { session },
-    error,
   } = await supabase.auth.getSession();
+  const { data, error } = await supabase.auth.getUser(session?.access_token);
 
   if (error) {
     console.error('세션 가져오기 오류:', error);
     await logout();
   }
 
-  if (error || !session?.user) {
+  if (error || !data?.user) {
     redirect('/login');
   }
 
-  return <p>안녕하세요, {session.user.email}님!</p>;
+  return <p>안녕하세요, {data.user.email}님!</p>;
 }
